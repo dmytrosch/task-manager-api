ad4f6b6749520b44da279d52a9d6c612afbf5c44
VVMachine <vik.atamanenko@gmail.com>
2021-02-09 15:07:20 +0200

    add asyncWrapper

--- API/users/users.controllers.js
+++ API/users/users.controllers.js
@@ -1,17 +1,9 @@
-const userModel = require("../users/users.model");
-const { UnauthorizedError } = require("../../helpers/error.helpers");
-
-
 class UsersControllers {
     async getCurrentUser(req, res, next) {
-        try {
-            const { email, projectIds } = req.user;
-            return res.status(200).send({ email, projectIds })
-        } catch(err) {
-            next(err)
-        }
-    }
+        const { email, projectIds } = req.user;
+        return res.status(200).send({ email, projectIds })
+}
 }
 
 
-module.exports = new UserControllers();
+module.exports = new UsersControllers();
--- API/users/users.router.js
+++ API/users/users.router.js
@@ -2,10 +2,14 @@ const { Router } = require("express");
 
 const autorization = require("../../helpers/authorization");
 const UsersControllers = require("./users.controllers");
+const asyncWrapper = require("../../utils/asyncWrapper");
 
 const userRouter = Router();
 
-userRouter.get("/current", autorization, UsersControllers.getCurrentUser)
-
+userRouter.get(
+  "/current",
+  autorization,
+  asyncWrapper(UsersControllers.getCurrentUser)
+);
 
 module.exports = userRouter;
