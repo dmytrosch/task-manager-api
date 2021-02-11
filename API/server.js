f308a6bb1ab9a0c79169dfa939d5ff039ab77aec
Dmytro.Sch <dmytro.sch@list.ru>
2021-02-11 14:59:27 +0200

    remove deprecated property of mongoose connection

--- API/server.js
+++ API/server.js
@@ -1,55 +1,55 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const authRouter = require("./auth/auth.router");
-const usersRouter = require("./users/users.router");
-const projectsRouter = require("./taskmgr/projects/projects.router");
-const sprintsRouter = require("./taskmgr/sprints/sprints.router");
+const authRouter = require('./auth/auth.router')
+const usersRouter = require('./users/users.router')
+const projectsRouter = require('./taskmgr/projects/projects.router')
+const sprintsRouter = require('./taskmgr/sprints/sprints.router')
 
 module.exports = class taskMgrServer {
   constructor() {
-    this.server = null;
-    this.SERVER_PORT = null;
+    this.server = null
+    this.SERVER_PORT = null
   }
 
   async start() {
     // Input start middlwares here
-    this.initPort();
-    this.initServer();
-    this.initMiddlwares();
-    this.initRoutes();
-    await this.initDatabase();
-    this.startListening();
+    this.initPort()
+    this.initServer()
+    this.initMiddlwares()
+    this.initRoutes()
+    await this.initDatabase()
+    this.startListening()
   }
 
   initServer() {
-    this.server = express();
-    console.log("server initialized");
+    this.server = express()
+    console.log('server initialized')
   }
 
   initPort() {
-    this.SERVER_PORT = process.env.PORT || 3000;
-    console.log("port initialized");
+    this.SERVER_PORT = process.env.PORT || 3000
+    console.log('port initialized')
   }
 
   initMiddlwares() {
-    this.server.use(express.json());
-    this.server.use(cors({ origin: `http://localhost:${process.env.PORT}` }));
-    this.server.use(morgan("dev"));
-    console.log("middlewares initialized");
+    this.server.use(express.json())
+    this.server.use(cors({ origin: `http://localhost:${process.env.PORT}` }))
+    this.server.use(morgan('dev'))
+    console.log('middlewares initialized')
   }
 
   initRoutes() {
     // input routers here
-    this.server.use("/api/auth", authRouter);
-    this.server.use("/api/users", usersRouter);
-    this.server.use('/api/projects', projectsRouter);
-    this.server.use('/api/sprints', sprintsRouter);
+    this.server.use('/api/auth', authRouter)
+    this.server.use('/api/users', usersRouter)
+    this.server.use('/api/projects', projectsRouter)
+    this.server.use('/api/sprints', sprintsRouter)
 
-    console.log("routes initialized");
+    console.log('routes initialized')
   }
 
   async initDatabase() {
@@ -57,19 +57,20 @@ module.exports = class taskMgrServer {
       await mongoose.connect(process.env.MONGO_DB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      });
-      console.log("Database connection successful");
+        useCreateIndex: true,
+      })
+      console.log('Database connection successful')
     } catch (err) {
-      console.log(err);
-      process.exit(1);
+      console.log(err)
+      process.exit(1)
     }
 
-    console.log("DB initialized");
+    console.log('DB initialized')
   }
 
   startListening() {
     this.server.listen(this.SERVER_PORT, () => {
-      console.log("Server started at PORT:", this.SERVER_PORT);
-    });
+      console.log('Server started at PORT:', this.SERVER_PORT)
+    })
   }
-};
+}
